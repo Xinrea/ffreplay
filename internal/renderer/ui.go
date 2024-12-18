e57baf17214c5736bdc224a831e000d190d23552
Xinrea <xinreas@outlook.com>
2024-12-18 11:31:25 +0800

    feat: formated progress time and hp

--- internal/renderer/ui.go
+++ internal/renderer/ui.go
@@ -3,6 +3,8 @@ package renderer
 import (
 	"fmt"
 	"image/color"
+	"strconv"
+	"strings"
 
 	"github.com/Xinrea/ffreplay/internal/component"
 	"github.com/Xinrea/ffreplay/internal/entry"
@@ -54,7 +56,7 @@ func (r *Renderer) UIRender(ecs *ecs.ECS, screen *ebiten.Image) {
 			r.EnemyCasting.Render(screen, healthRight-float64(r.EnemyCasting.w), float64(30+cnt*gap), p)
 		}
 		DrawText(screen, enemy.Name, 7, healthLeft, float64(20+cnt*gap), r.EnemyHealthBar.Color, AlignLeft)
-		DrawText(screen, fmt.Sprintf("HP: %d/%d", enemy.HP, enemy.MaxHP), 7, healthLeft, float64(50+cnt*gap), r.EnemyHealthBar.Color, AlignLeft)
+		DrawText(screen, fmt.Sprintf("HP: %s / %s", formatInt(enemy.HP), formatInt(enemy.MaxHP)), 7, healthLeft, float64(50+cnt*gap), r.EnemyHealthBar.Color, AlignLeft)
 		DrawText(screen, fmt.Sprintf("%.2f%%", percent*100), 7, healthRight, float64(50+cnt*gap), r.EnemyHealthBar.Color, AlignRight)
 		cnt++
 	}
@@ -70,7 +72,7 @@ func (r *Renderer) UIRender(ecs *ecs.ECS, screen *ebiten.Image) {
 	if global.FightDuration.Load() > 0 {
 		p = current / float64(global.FightDuration.Load())
 	}
-	DrawText(screen, fmt.Sprintf("%.1fs / %.1fs", current, float64(global.FightDuration.Load())/1000), 7, w-30, h-120, color.White, AlignRight)
+	DrawText(screen, fmt.Sprintf("%s / %s", formatDuration(current), formatDuration(float64(global.FightDuration.Load())/1000)), 7, w-30, h-120, color.White, AlignRight)
 	r.PlayProgress.Render(screen, w-float64(r.PlayProgress.w)-30, h-100, p)
 
 	// Draw shortkey prompt
@@ -78,4 +80,32 @@ func (r *Renderer) UIRender(ecs *ecs.ECS, screen *ebiten.Image) {
 	DrawText(screen, "快退: 方向键左 | 快进: 方向键右", 7, w-30, h-70, color.White, AlignRight)
 	DrawText(screen, "移动视角 W/A/S/D | 旋转视角: E/Q", 7, w-30, h-50, color.White, AlignRight)
 	DrawText(screen, "暂停: SPACE | 播放速度: 方向键（上下）| 回到开始: R", 7, w-30, h-30, color.White, AlignRight)
+}
+
+func formatDuration(s float64) string {
+	minutes := int(s) / 60
+	seconds := int(s) % 60
+	return fmt.Sprintf("%02d:%02d", minutes, seconds)
+}
+
+func formatInt(n int) string {
+	// 将 int64 转换为字符串
+	str := strconv.FormatInt(int64(n), 10)
+
+	// 计算整数的长度
+	length := len(str)
+	if length <= 3 {
+		return str // 如果长度小于等于3，直接返回
+	}
+
+	// 使用 strings.Builder 来构建结果字符串
+	var builder strings.Builder
+	for i, digit := range str {
+		// 每三位添加一个逗号
+		if i != 0 && (length-i)%3 == 0 {
+			builder.WriteRune(',')
+		}
+		builder.WriteRune(digit)
+	}
+	return builder.String()
 }
