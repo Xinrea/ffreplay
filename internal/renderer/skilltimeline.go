4ee4d2e2cee957e8f3a5515f86251ac782bd22fe
Xinrea <xinreas@outlook.com>
2025-01-02 22:37:24 +0800

    feat: HiDPI skill timeline render (close #13)

--- internal/renderer/skilltimeline.go
+++ internal/renderer/skilltimeline.go
@@ -37,36 +37,52 @@ func init() {
 }
 
 func initBackground() {
-	background = ebiten.NewImage(int(TIMELINE_WIDTH), int(TIMELINE_HEIGHT))
-	skillLayer = ebiten.NewImage(int(TIMELINE_WIDTH), int(TIMELINE_HEIGHT))
-	skillLayerMask = ebiten.NewImage(int(TIMELINE_WIDTH), int(TIMELINE_HEIGHT))
-	for i := 0; i < int(TIMELINE_WIDTH); i++ {
-		p := min(128, min(i, int(TIMELINE_WIDTH)-i))
-		for j := 0; j < int(TIMELINE_HEIGHT); j++ {
-			background.Set(i, j, color.NRGBA{0, 0, 0, uint8(p)})
+	s := ebiten.Monitor().DeviceScaleFactor()
+	background = ebiten.NewImage(int(TIMELINE_WIDTH*s), int(TIMELINE_HEIGHT*s))
+	skillLayer = ebiten.NewImage(int(TIMELINE_WIDTH*s), int(TIMELINE_HEIGHT*s))
+	skillLayerMask = ebiten.NewImage(int(TIMELINE_WIDTH*s), int(TIMELINE_HEIGHT*s))
+	for i := 0; i < int(TIMELINE_WIDTH*s); i++ {
+		p := 1.0
+		if i < int(TIMELINE_WIDTH*s*0.16) {
+			p = float64(i) / (TIMELINE_WIDTH * s * 0.16)
+		}
+		if i > int(TIMELINE_WIDTH*s*0.84) {
+			p = 1 - (float64(i)-TIMELINE_WIDTH*s*0.84)/(TIMELINE_WIDTH*s*0.16)
+		}
+		for j := 0; j < int(TIMELINE_HEIGHT*s); j++ {
+			background.Set(i, j, color.NRGBA{0, 0, 0, uint8(p * 128)})
 		}
 	}
-	for i := 0; i < int(TIMELINE_WIDTH); i++ {
-		p := min(255, min(i, int(TIMELINE_WIDTH)-i))
-		for j := 0; j < int(TIMELINE_HEIGHT); j++ {
-			skillLayerMask.Set(i, j, color.NRGBA{0, 0, 0, uint8(p)})
+	for i := 0; i < int(TIMELINE_WIDTH*s); i++ {
+		p := 1.0
+		if i < int(TIMELINE_WIDTH*s*0.16) {
+			p = float64(i) / (TIMELINE_WIDTH * s * 0.16)
+		}
+		if i > int(TIMELINE_WIDTH*s*0.84) {
+			p = 1 - (float64(i)-TIMELINE_WIDTH*s*0.84)/(TIMELINE_WIDTH*s*0.16)
+		}
+		for j := 0; j < int(TIMELINE_HEIGHT*s); j++ {
+			skillLayerMask.Set(i, j, color.NRGBA{0, 0, 0, uint8(p * 255)})
 		}
 	}
 }
 
 func RenderCasting(debug bool, canvas *ebiten.Image, tick int64, cast *model.Skill, x, y float64) {
-	textSize := 12.0
-	yOffset := 30.0
+	s := ebiten.Monitor().DeviceScaleFactor()
+	textSize := 12.0 * s
+	yOffset := 30.0 * s
 	iconTexture := cast.Texture()
 	geoM := texture.CenterGeoM(iconTexture)
+	geoM.Scale(s, s)
 	borderGeoM := model.BorderGeoM
+	borderGeoM.Scale(s, s)
 	if !cast.IsGCD {
 		geoM.Scale(0.8, 0.8)
 		borderGeoM.Scale(0.8, 0.8)
-		geoM.Translate(0, -30)
-		borderGeoM.Translate(0, -30)
-		textSize = 10.0
-		yOffset = -50
+		geoM.Translate(0, -30*s)
+		borderGeoM.Translate(0, -30*s)
+		textSize = 10.0 * s
+		yOffset = -55 * s
 	}
 	geoM.Translate(x, y)
 	borderGeoM.Translate(x, y)
@@ -80,13 +96,14 @@ func RenderCasting(debug bool, canvas *ebiten.Image, tick int64, cast *model.Ski
 }
 
 func (g GCDPeriod) Render(debug bool, canvas *ebiten.Image, x, y float64, tick int64) {
+	s := ebiten.Monitor().DeviceScaleFactor()
 	if g.GCDSkill != nil {
 		// tailLength := tickToLength(util.MSToTick(g.GCDSkill.Cast))
 		// DrawFilledRect(canvas, x, y-6, tailLength, 12, color.NRGBA{119, 123, 131, 255})
 		RenderCasting(debug, canvas, tick, g.GCDSkill, x, y)
 	}
 	for i := range g.NoneGCDSkills {
-		RenderCasting(debug, canvas, tick, g.NoneGCDSkills[i], x+float64(i+1)*NONEGCD_GAP, y)
+		RenderCasting(debug, canvas, tick, g.NoneGCDSkills[i], x+float64(i+1)*NONEGCD_GAP*s, y)
 	}
 }
 
@@ -128,22 +145,24 @@ func (st SkillTimeline) Render(debug bool, canvas *ebiten.Image, x, y float64, t
 	if len(st.Periods) == 0 {
 		return
 	}
-	if int(TIMELINE_WIDTH) != background.Bounds().Dx() {
+	if int(TIMELINE_WIDTH*s) != background.Bounds().Dx() {
 		initBackground()
 		log.Println("Scale factor changed, recreated skill timeline assets")
 	}
-	x = x - TIMELINE_WIDTH/2
+	x = x * s
+	y = y * s
+
+	x = x - TIMELINE_WIDTH*s/2
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(x, y)
-	op.GeoM.Scale(s, s)
 	canvas.DrawImage(background, op)
 
 	skillLayer.Clear()
 	for i := range st.Periods {
 		offset := tickToLength(tick - st.Periods[i].StartTick)
 		// draw seperator
-		px := -offset*1.3 + TIMELINE_WIDTH
-		py := TIMELINE_HEIGHT/2 + 20.0
+		px := -offset*1.3*s + TIMELINE_WIDTH*s
+		py := TIMELINE_HEIGHT*s/2 + 15.0*s
 		st.Periods[i].Render(debug, skillLayer, px, py, tick)
 	}
 	skillLayer.DrawImage(skillLayerMask, &ebiten.DrawImageOptions{Blend: ebiten.BlendDestinationIn})
